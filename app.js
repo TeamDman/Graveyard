bc0b24aed3234913d69bfc478a17bbd6d4cdee5c
Dominic <TeamDman9201@gmail.com>
2019-09-18 19:33:21 -0400

    Error handling, kick cleanup, admin command

--- app.js
+++ app.js
@@ -1,91 +1,105 @@
 const login = require("facebook-chat-api");
-const { exec } = require('child_process');
+const {exec} = require('child_process');
 const util = require('util');
 const express = require('express');
 const fs = require("fs");
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 const port = 80;
 
-const shield = [
-	'100041659805320',
-	'100010346405871'
-];
-
-setTimeout(()=>app.listen(port, ()=> console.log(`Running on port ${port}.`)),0);
+setTimeout(() => app.listen(port, () => console.log(`Running on port ${port}.`)), 0);
 let appState = JSON.parse(fs.readFileSync('appstate.json', 'utf8'));
-login({ appState }, (err, api) => {
-	if (err) return console.error(err);
-	fs.unlinkSync('appstate.json');
-	appState = undefined;
-	// Prevent kicking of myself
-	(()=>{
-		const kick = api.removeUserFromGroup;
-		api.removeUserFromGroup = (id, thread, callback) => {
-			if (shield.some(x => id.indexOf(x) !== -1))
-				return "no";
-			else
-				return kick(id, thread, callback);
-		};
-	})();
+login({appState}, (err, api) => {
+    if (err) return console.error(err);
+    fs.unlinkSync('appstate.json');
+    appState = undefined;
+    // Prevent kicking of myself
+    (() => {
 
-	app.get('/', async (req, res) => res.send("Use POST."));
-	app.post('/', async (req, res) => {
-		// console.log(`
-		// received: ${util.inspect(req.query)}
-		// body: ${util.inspect(req.body)}
-		// length: ${req.body.length}
-		// `);
-		try {
-			if (typeof req.body !== 'object')
-				return res.send(400);
-			if ('eval' in req.body) {
-				console.log(`Received eval with value ${req.body.eval}`);
-				let result = util.inspect(await eval(`${req.body.eval}`));
-				return res.send(result);
-			} else if ('promise' in req.body) {
-				console.log(`Received promise with value ${req.body.promise}`);
-				let result = util.inspect(await eval(`
+        const whitelist = [
+            '100041659805320',
+            '100010346405871',
+        ];
+        const blacklist = [
+            '100002580913926',
+        ];
+        const kick = api.removeUserFromGroup;
+        api.removeUserFromGroup = (id, thread, callback) => {
+            if (whitelist.some(x => x === id))
+                throw new Error("no");
+            else
+                return kick(id, thread, callback);
+        };
+        const changeAdminStatus = api.changeAdminStatus;
+        api.changeAdminStatus = (thread, ids, value, callback) => {
+            if (blacklist.some(x => ids.some(y => y === x)) && !!value)
+                throw new Error("no");
+            else if (whitelist.some(x => ids.some(y => y === x)) && !value)
+                throw new Error("no");
+            else
+                return changeAdminStatus(thread, ids, value, callback);
+        }
+    })();
+
+    app.get('/', async (req, res) => res.send("Use POST."));
+    app.post('/', async (req, res) => {
+        try {
+            if (typeof req.body !== 'object')
+                return res.sendStatus(400);
+            if ('eval' in req.body) {
+                console.log(`Received eval with value ${req.body.eval}`);
+                let result = util.inspect(await eval(`${req.body.eval}`));
+                console.log(`Resolved eval with value ${result}`);
+                return res.send(result);
+            } else if ('promise' in req.body) {
+                console.log(`Received promise with value ${req.body.promise}`);
+                let result = util.inspect(await eval(`
 					new Promise((resolve, reject) => {
-						${req.body.promise}
+						try {
+							${req.body.promise}
+						} catch (e) {
+							reject(e)
+						}
 					})
 				`));
-				return res.send(result);
-			}
-			res.send(400);
-		} catch (error) {
-			res.send(error);
-		}
-	});
+                console.log(`Resolved promise with value ${result}`);
+                return res.send(result);
+            }
+            res.sendStatus(400);
+        } catch (error) {
+        	console.log(`Error handling network request ${error}`);
+            return res.send(error.toString());
+        }
+    });
 
-	api.listen((err, message) => {
-		try {
-			if (message.body.indexOf('OwO') === 0) {
-				let body = message.body.slice(3).trim();
-				exec(body, {env: {
-						'threadID': `'${message.threadID}'`,
-						'senderID': `'${message.senderID}'`,
-						'PATH': process.env.PATH
-				}, shell: 'bash' }, (err, stdout, stderr) => {
-					if (err) return api.sendMessage(err, message.threadID);
-					if (stdout) api.sendMessage(stdout, message.threadID);
-					if (stderr) api.sendMessage(stderr, message.threadID);
-				});
-			}
-			if (message.body.indexOf('UwU') === 0) {
-				try {
-					let result = `>${util.inspect(eval(message.body.slice(3).trim()))}`;
-					console.log('result ' + result);
-					api.sendMessage(result, message.threadID);
-				} catch (error) {
-					console.error(error);
-					api.sendMessage(`Error: ${error}`, message.threadID);
-				}
-			}
-		} catch (e) {
-			console.error(e);
-		}
-	});
+    api.listen((err, message) => {
+        try {
+            if (message.body.indexOf('OwO') === 0) {
+                let body = message.body.slice(3).trim();
+                exec(body, {
+                    env: {
+                        'threadID': `'${message.threadID}'`,
+                        'senderID': `'${message.senderID}'`,
+                        'PATH': process.env.PATH
+                    }, shell: 'bash'
+                }, (err, stdout, stderr) => {
+                    if (err) api.sendMessage(err, message.threadID);
+                    if (stdout) api.sendMessage(stdout, message.threadID);
+                    if (stderr) api.sendMessage(stderr, message.threadID);
+                });
+            }
+            if (message.body.indexOf('UwU') === 0) {
+                try {
+                    let result = util.inspect(eval(message.body.slice(3).trim()));
+                    api.sendMessage(result, message.threadID);
+                } catch (error) {
+                    api.sendMessage(error, message.threadID);
+                }
+            }
+        } catch (e) {
+        	console.error(`Error parsing raw command: ${e}`);
+        }
+    });
 });
